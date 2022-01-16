bf272306945ba444f69906ad5c2042fb4acd9734
JiHongKim15 <15computer@hanmail.net>
2022-01-17 00:28:31 +0900

    create Front Main file

---
+++ mbti-front/src/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="Main">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
