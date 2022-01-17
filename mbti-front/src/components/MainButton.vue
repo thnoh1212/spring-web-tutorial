ed900309e65e39923c572afdb09a360ea28901b5
JiHongKim15 <15computer@hanmail.net>
2022-01-17 23:59:54 +0900

    create mainbutton JIHONG

---
+++ mbti-front/src/components/MainButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button>{{name}}</button>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class MainButton extends Vue{
+        name: string = 'JIHONG';
+    }
+</script>
