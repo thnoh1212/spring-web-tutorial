794c2621ce6ba7972c404e3b4c7488fd17181e1a
JiHongKim15 <15computer@hanmail.net>
2022-01-18 21:43:41 +0900

    update nameButton & main

--- mbti-front/src/views/Home.vue
+++ mbti-front/src/views/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="home">
-    <mainbutton></mainbutton>
+    <br/>
+    <nameButton name = "지혜"></nameButton>
+    <br/>
+    <nameButton name = "지홍"></nameButton>
+    <br/>
+    <nameButton name = "기원"></nameButton>
+    <br/>
+    <nameButton name = "태훈"></nameButton>
+    <br/>
+    <nameButton name = "소영"></nameButton>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import mainbutton from '../components/MainButton.vue';
+import nameButton from '../components/NameButton.vue';
 
 @Component({
   components: {
-    mainbutton,
+    nameButton,
   },
 })
 export default class Home extends Vue {}
